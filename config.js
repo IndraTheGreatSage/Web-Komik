c58325ea8801be441892d22e3d5fdea0cbc40bde
33des <niparsia433@gmail.com>
2026-06-11 19:49:12 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.KOMIK_CONFIG = {
     mode: "api",
     // Ganti dengan URL API Vercel hasil deploy-an kamu sendiri
-    apiBaseUrl: "https://komiku-api-fix.vercel.app/",
+    apiBaseUrl: "https://komiku-api-fix.vercel.app",
     dataUrl: "komik-data.json",
 };
